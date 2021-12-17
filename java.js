5f4e23b1f08594d13f595dba7c9c2710c5577896
anupammaurya6767 <96181413+anupammaurya6767@users.noreply.github.com>
2021-12-17 20:42:19 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+document.getElementById("menubtn").onclick=function(){
+    document.getElementById("menubar").style.display="block";
+}
+
+document.getElementById("close").onclick=function(){
+     document.getElementById("menubar").style.display="none";
+}
