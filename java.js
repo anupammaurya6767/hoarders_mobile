899ae33269ef3ede4caab2e32c7c53ce51ca16f2
anupammaurya6767 <96181413+anupammaurya6767@users.noreply.github.com>
2021-12-18 17:52:41 +0530

    Delete java.js

--- java.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("menubtn").onclick=function(){
-    document.getElementById("menubar").style.display="block";
-}
-
-document.getElementById("close").onclick=function(){
-     document.getElementById("menubar").style.display="none";
-}
